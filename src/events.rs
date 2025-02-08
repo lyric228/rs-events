888976e93e9b55852de1f7c1ec81586c4aea5399
lyric228 <xhikzerox@gmail.com>
2025-02-09 01:12:56 +0500

    Added docs (ru)
    Added docs (ru)
    Little changes

--- src/events.rs
+++ src/events.rs
@@ -5,11 +5,15 @@ pub use std::any::Any;
 
 
 lazy_static! {
-    pub static ref EMITTERS: Mutex<HashMap<String, EventEmitter>> = Mutex::new(HashMap::new());
+    pub static ref EMITTERS: Mutex<HashMap<String, EventEmitter>> = {
+        let mut map = HashMap::new();
+        map.insert(String::from("default"), EventEmitter::new());
+        Mutex::new(map)
+    };
 }
 
 thread_local! {
-    pub static CURRENT_EMITTER_ID: std::cell::RefCell<String> = 
+    pub static CURRENT_EMITTER_ID: std::cell::RefCell<String> =
         std::cell::RefCell::new(String::from("default"));
 }
 
@@ -59,25 +63,67 @@ impl EventEmitter {
     }
 }
 
-#[macro_export]
-macro_rules! use_emitter {
-    ($id:expr) => {
-        $crate::CURRENT_EMITTER_ID.with(|cell| {
-            *cell.borrow_mut() = String::from($id);
-        });
-    };
-}
-
+#[doc="
+    Создаёт новый emitter с указанным идентификатором, если его ещё не существует.\n
+    В отличие от use_emitter! не изменяет текущий emitter.\n\n
+    Пример использования:\n
+    ```rust
+    new_emitter!(\"custom_emitter\");\n
+    ```\n
+    После создания можно переключиться на него с помощью use_emitter!
+"]
 #[macro_export]
 macro_rules! new_emitter {
     ($id:expr) => {
-        $crate::EMITTERS.lock().unwrap().insert(
-            String::from($id),
-            $crate::EventEmitter::new()
-        );
+        $crate::EMITTERS.lock().unwrap()
+            .entry(String::from($id))
+            .or_insert_with(|| $crate::EventEmitter::new());
     };
 }
 
+#[doc="
+    Устанавливает текущий emitter по переданному идентификатору и возвращает его.\n
+    Если указанный emitter не существует, он будет создан.\n\n
+    Пример использования:\n
+    ```rust
+    use_emitter!(\"main_emitter\");\n
+    // Все последующие вызовы on!/emit! будут использовать этот emitter\n
+    ```\n
+    Для возврата к default emitter:\n
+    ```rust
+    use_emitter!(\"default\");\n
+    ```
+"]
+#[macro_export]
+macro_rules! use_emitter {
+    ($id:expr) => {{
+        $crate::CURRENT_EMITTER_ID.with(|cell| {
+            *cell.borrow_mut() = String::from($id);
+        });
+        
+        $crate::EMITTERS.lock().unwrap()
+            .entry(String::from($id))
+            .or_insert_with(|| $crate::EventEmitter::new());
+    }};
+}
+
+#[doc="
+    Регистрирует обработчик для указанного события.\n
+    Поддерживает два варианта использования:\n
+    1. Без аргументов:\n
+    ```rust
+    on!(\"event_name\", || {
+        println!(\"Event handled!\");
+    });
+    ```\n
+    2. С аргументами определённых типов:\n
+    ```rust
+    on!(\"event_name\", |arg1: i32, arg2: String| {
+        println!(\"Received {} and {}\", arg1, arg2);
+    });
+    ```\n
+    Аргументы автоматически приводятся к указанным типам при вызове обработчика.
+"]
 #[macro_export]
 macro_rules! on {
     ($event:expr, || $body:expr) => {
@@ -112,6 +158,21 @@ macro_rules! on {
     };
 }
 
+#[doc="
+    Вызывает событие с именем `event`, передавая ему аргументы.\n\n
+    Примеры использования:\n
+    Без аргументов:\n
+    ```rust
+    emit!(\"start_event\");\n
+    ```\n
+    С аргументами разных типов:\n
+    ```rust
+    let num = 42;\n
+    let text = String::from(\"message\");\n
+    emit!(\"data_event\", num, text);\n
+    ```\n
+    Аргументы должны реализовывать трейт Any.
+"]
 #[macro_export]
 macro_rules! emit {
     ($event:expr $(, $arg:expr)*) => {
@@ -127,6 +188,16 @@ macro_rules! emit {
     };
 }
 
+#[doc="
+    Удаляет все обработчики для указанного события.\n
+    Пример использования:\n
+
+    ```rust
+    off!(\"event_name\");
+    ```\n
+
+    После вызова этого макроса событие перестанет обрабатываться.
+"]
 #[macro_export]
 macro_rules! off {
     ($event:expr) => {
